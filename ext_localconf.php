7f87007ce77e7241016b8fbe8ed9128345cab937
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-03-13 19:09:31 +0100

    Fixed cachable state Also lots of non-changes, seriously, fuck git EOL handling

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,6 @@
 if( !defined( 'TYPO3_MODE' ) ) {
 	die( 'Access denied.' );
 }
-Tx_Extbase_Utility_Extension::configurePlugin(  'downloads',  'Downloads',  array(    'Downloads' => 'list,download'  ),  array(    'Downloads' => ''  ));
-Tx_Extbase_Utility_Extension::configurePlugin(  'downloads',  'Stats',  array(    'Stats' => 'show,graphByDate,graphByCount,graphByReferrer'  ),  array(    'Stats' => ''  ));
+Tx_Extbase_Utility_Extension::configurePlugin(  'downloads',  'Downloads',  array(    'Downloads' => 'list'  ),  array(    'Downloads' => 'download'  ));
+Tx_Extbase_Utility_Extension::configurePlugin(  'downloads',  'Stats',  array(    'Stats' => ''  ),  array(    'Stats' => 'show,graphByDate,graphByCount,graphByReferrer'  ));
 ?>
